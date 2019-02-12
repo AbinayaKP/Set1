053be2e889f88bdfa0c8db7d60e9e6274c4977d3
AbinayaKP <46812407+AbinayaKP@users.noreply.github.com>
2019-02-12 22:20:03 +0530

    Update primerange.py

--- primerange.py
+++ primerange.py
@@ -2,12 +2,7 @@ x=input()
 y=input()
 count=0
 for num in range(x,y+1):
-    if num>1:
-        for i in range(2,num):
-            if (num%i==0):
-                break
-            else:
-                print(num)
-                count+=1
+    if all(num%i!=0 for i in range(2,num)):
+        count+=1
 print(count)
     
