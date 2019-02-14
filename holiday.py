12afea059e60296c2d2c238242bf86b162f8ca4c
AbinayaKP <46812407+AbinayaKP@users.noreply.github.com>
2019-02-14 22:59:16 +0530

    Update holiday.py

--- holiday.py
+++ holiday.py
@@ -1,7 +1,7 @@
 x=raw_input()
 if(x=='Monday'or x=="Tuseday" or x=="Wednesday" or x=="Thursday" or x=="Friday"):
-    print("yes")
-elif(x=="Sunday" or x=="Saturday"):
     print("no")
+elif(x=="Sunday" or x=="Saturday"):
+    print("yes")
 else:
     print("Not valid")
