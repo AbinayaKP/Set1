b11bc2f96ebfaf008a8c7b03882f3151878c4c50
AbinayaKP <46812407+AbinayaKP@users.noreply.github.com>
2019-02-14 23:53:06 +0530

    Update revvowel.py

--- revvowel.py
+++ revvowel.py
@@ -1,7 +1,7 @@
 x=raw_input()
 y=raw_input()
 for i in y:
-    if(i=="a" or i=="o" or i=="e" or i="i" or i=="u"):
+    if(i=="a" or i=="o" or i=="e" or i=="i" or i=="u"):
         count=0
     else:
         print i
