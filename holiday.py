858b103d6e12d29d72b1468330b6fd4b4afa0c31
AbinayaKP <46812407+AbinayaKP@users.noreply.github.com>
2019-02-14 22:57:59 +0530

    Create holiday.py

---
+++ holiday.py
@@ -0,0 +1,7 @@
+x=raw_input()
+if(x=='Monday'or x=="Tuseday" or x=="Wednesday" or x=="Thursday" or x=="Friday"):
+    print("yes")
+elif(x=="Sunday" or x=="Saturday"):
+    print("no")
+else:
+    print("Not valid")
