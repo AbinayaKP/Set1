17ad3647008f27ff79f80e2864519e47227bf5b2
AbinayaKP <46812407+AbinayaKP@users.noreply.github.com>
2019-02-15 17:13:56 +0530

    Update revvowel.py

--- revvowel.py
+++ revvowel.py
@@ -2,7 +2,7 @@ x=raw_input()
 y=raw_input()
 def anti_vowel(text):
     for i in "aeiouAEIOU":
-        text=text.replace(i,' ')
+        text=text.replace(i,'')
     return text
 z=anti_vowel(y)
 w=z[::-1]
