fab2cd16239b765a1d21b84fb7aff0b71db07d84
AbinayaKP <46812407+AbinayaKP@users.noreply.github.com>
2019-06-06 08:49:38 +0530

    Create proset13.py

---
+++ proset13.py
@@ -0,0 +1,11 @@
+#Abinaya
+m,n=input().split()
+ob=abs(len(m)-len(n))
+for i in range(len(m)):
+	if len(n)==1 and n[i] in m:
+		break
+	elif i>=len(m) or i>=len(n):
+		break
+	elif n[i]!=n[i] and n[i]:
+		ob=ob+1
+print(ob)
